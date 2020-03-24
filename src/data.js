0b1a853d27c5ff349c852fd60331ab4c1c4a2eef
Gaby <gabriel.fougerolle@ynov.com>
2020-03-24 15:54:48 +0100

    add firebase

--- src/data.js
+++ src/data.js
@@ -9,12 +9,9 @@ export const storeProducts = [
     title: "Sac lucyan",
     img: "img/image.jpg",
     price: 10,
-    company: "Raymonde",
-    info:
+    Description:
       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
+    like: 0,
   },
   {
     id: 2,
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyD-q-Vu0UQ32E0UpHBWirFan10mkYItrnY",
+    authDomain: "lucyan-b7589.firebaseapp.com",
+    databaseURL: "https://lucyan-b7589.firebaseio.com",
+    projectId: "lucyan-b7589",
+    storageBucket: "lucyan-b7589.appspot.com",
+    messagingSenderId: "797431556799"
+}
