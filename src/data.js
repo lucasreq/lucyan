41f0c980194538e1ee5a4ab5d65d2ed89355043c
Gaby <gabriel.fougerolle@ynov.com>
2020-03-29 20:16:36 +0200

    bdd encore cassé

--- src/data.js
+++ src/data.js
@@ -1,113 +1,50 @@
-/* 
-https://www.iconfinder.com/icons/1243689/call_phone_icon
-Creative Commons (Attribution 3.0 Unported);
-https://www.iconfinder.com/Makoto_msk */
+import * as firebase from 'firebase';
+import config from'./firebase';
+import './App.css';
 
-export const storeProducts = [
-  {
-    id: 1,
-    title: "Sac lucyan",
-    img: "img/image.jpg",
-    price: 10,
-    Description:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    like: 0,
-  },
-  {
-    id: 2,
-    title: "Sac lucyan",
-    img: "img/image.jpg",
-    price: 16,
-    company: "Test1",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 3,
-    title: "Sac lucyan",
-    img: "img/image.jpg",
-    price: 8,
-    company: "test2",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 4,
-    title: "Sac lucyan",
-    img: "img/image.jpg",
-    price: 18,
-    company: "jean-claude",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 5,
-    title: "Sac lucyan",
-    img: "img/image.jpg",
-    price: 24,
-    company: "pouet",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 6,
-    title: "Sac lucyan",
-    img: "img/image.jpg",
-    price: 17,
-    company: "Heyo",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 7,
-    title: "Sac lucyan",
-    img: "img/image.jpg",
-    price: 30,
-    company: "Tropb1",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 8,
-    title: "Sac lucyan",
-    img: "img/image.jpg",
-    price: 2,
-    company: "Un fabriquant de sac",
-    info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
-    count: 0,
-    total: 0
+class Data extends Component {
+  constructor() {
+    super()
+
+    this.state= {loading: true}
+    if (!firebase.apps.length) {
+      firebase.initializeApp(config);
   }
-];
+  }
 
-export const detailProduct = {
-  id: 1,
-  title: "Sac lucyan",
-  img: "img/image.jpg",
-  price: 10,
-  company: "Raymonde",
-  info:
-    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-  inCart: false,
-  count: 0,
-  total: 0
-};
+  componentWillMount() {
+    const SacRef = firebase.database().ref('sac')
+
+    SacRef.on('value', snapshot =>{
+      this.setState({
+        sac: snapshot.val(),
+        loading: false
+      })
+    })
+  }
+
+  render() {
+    if (this.state.loading) {
+      return(
+        //ici tu peu mettre ce que tu veux qui vas signifier que la bdd ne charge pas
+        <p>je charge</p>
+      )
+    }
+    var storeProduct = [];
+    const sacs = Object.keys(this.state.sac).map(key => {
+      var tableaudemerde = {
+        id: {key},
+        name: {this.state.sac[key].name},
+        description: {this.state.sac[key].description},
+        price: {this.state.sac[key].price},
+        like: {this.state.sac[key].like},
+        image: {this.state.sac[key].image}
+      }
+    })
+    return (
+      <div>{sacs}</div>
+    )
+  }
+}
+
+export default Data
